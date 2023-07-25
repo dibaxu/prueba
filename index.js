131ba6a0adfd790986e50cef6dff1c1514d07c5e
[Pedro Sota] <psptaier@gmail.com>
2023-07-25 19:24:26 -0300

    primer commit para probar el slide

---
+++ index.js
@@ -0,0 +1,68 @@
+const track = document.getElementById("image-track");
+
+const handleOnDown = e => track.dataset.mouseDownAt = e.clientX;
+const handleOnUp = () => {
+    track.dataset.mouseDownAt = "0";
+    track.dataset.prevPercentage = track.dataset.percentage;
+}
+const handleOnMove = e => {
+    if(track.dataset.mouseDownAt === "0") return;
+
+    const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX, maxDelta = window.innerWidth / 2;
+
+    const percentage = (mouseDelta / maxDelta) * -100, 
+        nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage, 
+        nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
+        
+    track.dataset.percentage = nextPercentage;
+
+    track.animate({
+        transform: "translate(${nextPercentage}%, -50%)"}, {duration: 1200, fill: "forwards"} );
+    
+
+    for(const image of track.getElementsByClassName("image")) {
+        image.animate(
+            {objectPosition: '${nextPercentage + 100}% center'}, {duration: 1200, fill: "forwards"}
+        )        
+    }   
+
+}
+
+window.onmousedown = e => handleOnDown(e);
+
+window.ontouchstart = e => handleOnDown(e.touches[0]);
+
+window.onmouseup = e => handleOnUp(e);
+
+window.ontouchend = e => handleOnUp(e.touches[0]);
+
+window.onmousemove = e => handleOnMove(e);
+
+window.ontouchmove = e => handleOnMove(e.touches[0]);
+
+
+
+// window.onmouseup = () => {
+//     track.dataset.mouseDownAt = "0";
+//     track.dataset.prevPercentage = track.dataset.percentage;
+// }
+
+// window.onmousemove = e => {
+//     if(track.dataset.mouseDownAt === "0") return;
+
+//     const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX, maxDelta = window.innerWidth / 2;
+
+//     const percentage = (mouseDelta / maxDelta) * -100, nextPercentage = parseFloat(track.dataset.prevPercentage) + percentage;
+
+//     track.dataset.percentage = nextPercentage;
+//     track.animate({
+//         transform: "translate(${nextPercentage}%, -50%)"}, {duration: 1200, fill: "forwards"} );
+    
+
+//     for(const image of track.getElementsByClassName("image")) {
+//         image.animate(
+//             {objectPosition: '${nextPercentage + 100}% center'}, {duration: 1200, fill: "forwards"}
+//         )
+        
+//     }
+// }
